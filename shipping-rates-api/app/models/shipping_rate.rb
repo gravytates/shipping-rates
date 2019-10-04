cabe7a9186d4725ce1c6303389b6f019e2cd1d5a
Grady Shelton <gradyish@gmail.com>
2019-10-04 10:04:44 -0700

    Add csv import gem and  methods for models.

--- shipping-rates-api/app/models/shipping_rate.rb
+++ shipping-rates-api/app/models/shipping_rate.rb
@@ -1,2 +1,16 @@
 class ShippingRate < ApplicationRecord
+  require 'csv'
+  require 'activerecord-import/base'
+  require 'activerecord-import/active_record/adapters/postgresql_adapter'
+
+  def self.csv_import(file)
+    shipping_rates = []
+    CSV.foreach(file.path, headers: true) do |row|
+
+      formatted_row = [["origin", "destination", "rate", "currency", "shipping_company_id"],
+                [row[1][2], row[1][3], row[1][0], row[1][1], row[1][4]]]
+
+      shipping_rates << ShippingRate.new(formatted_row.to_h)
+    end
+    ShippingRate.import shipping_rates, recursive: true
 end
--- shipping-rates-api/app/models/shipping_service_provider.rb
+++ shipping-rates-api/app/models/shipping_service_provider.rb
@@ -1,2 +1,15 @@
 class ShippingServiceProvider < ApplicationRecord
+  require 'csv'
+  require 'activerecord-import/base'
+  require 'activerecord-import/active_record/adapters/postgresql_adapter'
+
+  def self.csv_import(file)
+    shipping_service_providers = []
+    CSV.foreach(file.path, headers: true) do |row|
+      formatted_row = [["name", "flat_rate", "currency"],
+                      [row[1][1], row[1][2], row[1][3]]]
+
+      shipping_service_providers << ShippingServiceProvider.new(formatted_row.to_h)
+    end
+    ShippingServiceProvider.import shipping_service_providers, recursive: true
 end
